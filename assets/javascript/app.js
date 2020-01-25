e1fbab265f5c3ff54f07c569d6f4d1c44add3831
Mai Houa Vue <mhvue4@gmail.com>
2020-01-25 13:50:24 -0600

    tested search by book titles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,21 @@
 //main JS file
 
+
 //NYT API
 var mvAPI = "XrPZZH0SkeXWEk4ExM3vIM4gh2neOKwv";
 // // NYT testing API Key. the use of  "current" which means getting the latest list 
-//     var queryURL="https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=" + mvAPI;
-//     console.log(queryURL);
+    var queryURL="https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=" + mvAPI;
+    console.log(queryURL);
 
-// //testing looking up a Title (with title stored in a var)
-var bookTitle= "Becoming"
-var queryURL="https://api.nytimes.com/svc/books/v3/reviews.json?title=" + bookTitle + "&api-key=" + mvAPI;
-console.log(queryURL);
-
-// //testing looking up a Author (with author stored in a var)
-// var author= "Stephen King"
-// var queryURL="https://api.nytimes.com/svc/books/v3/reviews.json?author=" + author + "&api-key=" + mvAPI;
+// // //testing looking up a Title (with title stored in a var)
+// var bookTitle= "Mostly Dead Things"
+// var queryURL="https://api.nytimes.com/svc/books/v3/reviews.json?title=" + bookTitle + "&api-key=" + mvAPI;
 // console.log(queryURL);
+
+// // //testing looking up a Author (with author stored in a var)
+// // var author= "Stephen King"
+// // var queryURL="https://api.nytimes.com/svc/books/v3/reviews.json?author="+author+"&api-key="+mvAPI;
+// // console.log(queryURL);
 
 
 $.ajax({
