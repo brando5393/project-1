5a4b19ce69f532097c15a7d32cb6b737edc7ce59
Dylan Myers <dylanmyers178@gmail.com>
2020-01-27 21:34:18 -0600

    Is not working but I think most of the javaScript is here Just need to finish the for loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,49 @@
 
 
-// Google books API 
+        // Google books API 
 var term = "a man in moscow"
-
+        // Google Books URL
 var gBooksURL = "https://www.googleapis.com/books/v1/volumes?api_key=AIzaSyC_kBKxX1bOeYZ9z3Itd5x86QwbLL-uS_8&q=" + term 
-// Ajax for Google Books
+        // Ajax for Google Books
 $.ajax({
     url: gBooksURL,
     method: "GET"
 }).then(function(response) {
-    //console.log(response);
+        //console.log(response);
 
 
- 
-var searchResults = response.items
-console.log(searchResults);
+        // Var for response
+  var searchResults = response.items
+  console.log(searchResults);
 
 
-
+          //for loop for results needs to be updated to limit response to 10 results and then needs to have a clear function as well as links to HTML added (this is not working yet)
   for(var i = 0; i < searchResults; i++) {
-    //console.log(searchResults[i]);
-   
+    
+
+
+
+          //Variables for results
+    var title = searchResults.volumeInfo.title;
+    console.log(title);
+    var author = searchResults.volumeInfo.author;
+    console.log(author);
+    var date = searchResults.volumeInfo.publishDate;
+    console.log(date);
+    var descript = searchResults.volumeInfo.discription;
+    console.log(descript);
+    var image = searchResults.volumeInfo.imageLinks.smallThumbnail
+    console.log(image);
+
+
+
+
+
+    console.log(searchResults.volumeInfo.title);
+    console.log(SearchResults.volumeInfo.authors);
+    console.log(SearchResults.volumeInfo.publishDate);
+    console.log(SearchResults.volumeInfo.description);
+    console.log(searchresults.columeInfo.imageLinks.smallThumbnail);
 
 
 
@@ -28,28 +51,19 @@ console.log(searchResults);
 
 
 
-    // console.log(searchResults.volumeInfo.title);
-    // console.log(SearchResults.volumeInfo.authors);
-    // console.log(SearchResults.volumeInfo.publishDate);
-    // console.log(SearchResults.volumeInfo.description);
+            //Sends results to HTML
+    var yourResults = $("<p>").html(
+      "Title: " + title + "<br>" +
+      "Author: " + author + "<br>" +
+      "Description: " + discript + "<br>" 
+    )
 
+            //IMG tag for book image
+    // $("").html("<img>" + searchResults.volumeinfo.imageLinks.smallThumbnail);
 
-
-
-
-
-
-
-
-    // $("").html("<p>" + searchResults.title);
-    // $("").html("<p>" + searchResults.volumeInfo.authors);
-    // $("").html("<p>" + searchResults.volumeInfo.publishDate);
-    // $("").html("<p>" + searchResults.volumeInfo.description);
-
-
-
-
-  }; 
+    //$(div name).append()
+  };
+  
 
 
 });
