4b64a9c7e821d91650b1afa1f4a7eb8a60da73b3
Dylan Myers <dylanmyers178@gmail.com>
2020-01-27 18:07:53 -0600

    pulling updates!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,19 @@
-//main JS file
+
+
+
+var term = "harry potter"
+
+var gBooksURL = "https://www.googleapis.com/books/v1/volumes?api_key=AIzaSyC_kBKxX1bOeYZ9z3Itd5x86QwbLL-uS_8&q=" + term 
+
+$.ajax({
+    url: gBooksURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(gBooksURL);
+    console.log(response);
+
+    $("").html("<h3>" + response.volumeInfo.title)
+    $("").html("<p>" + response.volumeInfo.authors)
+    $("").html("<p>" + response.volumeInfo.publishDate)
+    $("").html("<p>" + response.volumeInfo.description)
+});
