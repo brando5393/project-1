42b2e174de3c36b000af890ffdfcb24fa47c6e4b
Dylan Myers <dylanmyers178@gmail.com>
2020-01-27 20:58:10 -0600

    google api is pulling results need to limit the results to 10 in the for loop and add to HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,55 @@
 
 
-
-var term = "harry potter"
+// Google books API 
+var term = "a man in moscow"
 
 var gBooksURL = "https://www.googleapis.com/books/v1/volumes?api_key=AIzaSyC_kBKxX1bOeYZ9z3Itd5x86QwbLL-uS_8&q=" + term 
-
+// Ajax for Google Books
 $.ajax({
     url: gBooksURL,
     method: "GET"
 }).then(function(response) {
-    console.log(gBooksURL);
-    console.log(response);
+    //console.log(response);
 
-    $("").html("<h3>" + response.volumeInfo.title)
-    $("").html("<p>" + response.volumeInfo.authors)
-    $("").html("<p>" + response.volumeInfo.publishDate)
-    $("").html("<p>" + response.volumeInfo.description)
+
+ 
+var searchResults = response.items
+console.log(searchResults);
+
+
+
+  for(var i = 0; i < searchResults; i++) {
+    //console.log(searchResults[i]);
+   
+
+
+
+
+
+
+
+    // console.log(searchResults.volumeInfo.title);
+    // console.log(SearchResults.volumeInfo.authors);
+    // console.log(SearchResults.volumeInfo.publishDate);
+    // console.log(SearchResults.volumeInfo.description);
+
+
+
+
+
+
+
+
+
+    // $("").html("<p>" + searchResults.title);
+    // $("").html("<p>" + searchResults.volumeInfo.authors);
+    // $("").html("<p>" + searchResults.volumeInfo.publishDate);
+    // $("").html("<p>" + searchResults.volumeInfo.description);
+
+
+
+
+  }; 
+
+
 });
