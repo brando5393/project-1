892a47d2b200b77663cc7650bb6f538efe9674b4
Mai Houa Vue <mhvue4@gmail.com>
2020-01-27 21:32:55 -0600

    generate random  bestsellers book

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,15 +29,8 @@ $.ajax({
 
 
     // need to create a for loop that will only pull 5 random books  out of all these to show 
-    //I"m thinking will need to set up a new loop that will pull up 5 books 
-
-    var randomBook = bestsellers[Math.floor(Math.random() * bestsellers.length)];
-    console.log(randomBook);
-    
-
-    //this will pull up ALL the listings of books which is 15 and i want it t to show 5 RANDOM books
     for(var i=0; i<5; i++) {
-    // for(var i=0; i< bestsellers.length; i++) {
+    // for(var i=0; i< bestsellers.length; i++) { //this will pull up ALL the listings of books which is 15
     //     // console.log(bestsellers[i]);
 
 
@@ -52,21 +45,35 @@ $.ajax({
         var ratings=bestsellers[i].rank;
         // console.log(ratings);
 
+
+        var randomBook = bestsellers[Math.floor(Math.random() * bestsellers.length)];
+        // console.log(randomBook.title);
+
+        var ranBookTitle= randomBook.title;
+        console.log(ranBookTitle);
+        var ranBookAuth=randomBook.author;
+        var ranBookImg=randomBook.book_image;
+        var ranBookSynp= randomBook.description;
+        var ranBookRating= randomBook.rank;
+             
+        // for (var j=0; j < randomBook.length; j++) {
+        //     console.log(randomBook[0])
+        // }
     
         //created Img tag for  bookImg
-        var booksImgHolder = $("<img>").attr("src",bookImg).addClass("bestSellersImg")
-        console.log(booksImgHolder);
+        var booksImgHolder = $("<img>").attr("src",ranBookImg).addClass("bestSellersImg")
+        // console.log(booksImgHolder);
 
         //all the info to be display 
         var bestSellersInfo= $("<p>").html(
-            "Rank " + ratings  + " out of 15" + "<br>" + //have explain NYT have 15 total somewhere
-            "Title: " + title + "<br>" + 
-            "Author: " + author + "<br>"  +
-            "Synopsis: " + bookSynp + "<br>" 
+            "Rank " + ranBookRating  + " out of 15" + "<br>" + //have explain NYT have 15 total somewhere
+            "Title: " + ranBookTitle + "<br>" + 
+            "Author: " + ranBookAuth + "<br>"  +
+            "Synopsis: " + ranBookSynp + "<br>" 
              );
 
    
-
+     
     //have to get the information want to display associated with the random books shown..
         //this does show random books (only displaying 5 b/c of loop from above);
 
