3b60b75b31838508bdc4d7a1756598339f2ae88f
Mai Houa Vue <mhvue4@gmail.com>
2020-01-29 20:34:29 -0600

    took out functions-not needed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //main JS file
 
 
@@ -18,29 +17,9 @@ $.ajax({
     // console.log(response.results.books);//this gives list of all info of bestsellers in an array
     var bestsellers = response.results.books //storing bestsellers in a var
 
-    var invervalid;
 
     var usedNum= {};
 
-    function ranBookTime () {
-        setTimeout (startRandom, 3000);
-    };
-    ranBookTime();
-    
-    function nextBooks () {
-        setInterval (startRandom, 3000);
-        reset();
-    
-    };
-    
-    function reset () {
-        $(".test-div").empty();
-        
-    }
-
-    
-    function startRandom () {
-        nextBooks();
     // need to create a for loop that will only pull 5 random books  out of all these to show 
     for(var i=0; i<5; i++) {
     // for(var i=0; i< bestsellers.length; i++) { //this will pull up ALL the listings of books which is 15
@@ -87,61 +66,10 @@ $.ajax({
              );
 
        //testing if it'll show on HTML ...added test-div in html to test this out for now
-     $(".test-div").append(booksImgHolder, bestSellersInfo);
+     $("#best-sellers-container").prepend(booksImgHolder, bestSellersInfo);
     };
 
-}; //closure for function start 
+}); 
 
 
-});
-=======
-// firebase connect code
-$(document).ready(function(){
-  $('.collapsible').collapsible();
-});
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyCtuSfxpVxINhIyPIpuMGAHswNVvPYAgeM",
-    authDomain: "coding-bootcamp-project-986ec.firebaseapp.com",
-    databaseURL: "https://coding-bootcamp-project-986ec.firebaseio.com",
-    projectId: "coding-bootcamp-project-986ec",
-    storageBucket: "coding-bootcamp-project-986ec.appspot.com",
-    messagingSenderId: "25197836680",
-    appId: "1:25197836680:web:08de31c393589bc2cfe23d"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-  // "I was thinking that this will be data that will go into rows inside the Your Results container?"
-  
-  var createRow = function(data) {
-    // Create a new table row element
-    var tRow = $("<tr>");
-
-  //   // Methods run on jQuery selectors return the selector they we run on
-  // //   // This is why we can create and save a reference to a td in the same statement we update its text
-  //   var titleTd = $("<td>").text(data.Title);
-  //   var yearTd = $("<td>").text(data.Year);
-  //   var authorTd = $("<td>").text(data.Author);
-  //   // Append the newly created table data to the table row
-  //   tRow.append(titleTd, yearTd, authorTd);
-  //   // Append the table row to the table body
-  //   $("tbody").append(tRow);
-  // };
-
-  // // , searche the ? api for books, and then pass the data to createRow
-  // var searchBooks= function(books) {
-  //   var queryURL = "https://www.omdbapi.com/?t=" + books + "&apikey=trilogy";
-  //   $.ajax({
-  //     url: queryURL,
-  //     method: "GET"
-  //   }).then(function(response) {
-  //     createRow(response);
-  //   });
-  // };
-
-  // // Search the API for the following book, and append table rows for each
-  // searchBooks("");
- 
->>>>>>> 2680e1e0cc6f123355e04b104dbe0ef1bca0f7e3
 
