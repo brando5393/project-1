786541aa303cb01c1b40cf2a149f46daaeb5a633
Brandon Williams <brando5393@gmail.com>
2020-02-01 14:11:10 -0600

    pushing js updates

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,99 +1,70 @@
-var firebaseConfig = {
+var config = {
     apiKey: "AIzaSyCtuSfxpVxINhIyPIpuMGAHswNVvPYAgeM",
-    authDomain: "coding-bootcamp-project-986ec.firebaseapp.com",
-    databaseURL: "https://coding-bootcamp-project-986ec.firebaseio.com",
-    projectId: "coding-bootcamp-project-986ec",
-    storageBucket: "coding-bootcamp-project-986ec.appspot.com",
-    messagingSenderId: "25197836680",
-    appId: "1:25197836680:web:08de31c393589bc2cfe23d"
+    apiKey: "AIzaSyD7dRf0QJCxGudbWuvwIcAggJY5kc_SGe4",
+    authDomain: "project1-5bfdb.firebaseapp.com",
+    databaseURL: "https://project1-5bfdb.firebaseio.com",
+    projectId: "project1-5bfdb",
+    storageBucket: "project1-5bfdb.appspot.com",
+    messagingSenderId: "295028971332",
+    appId: "1:295028971332:web:9f200f96f1c9de421f6911"
   };
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(config);
   
   
   var database = firebase.database();
   
-  
-  
-  // firebase code to store reading lists
-  database.ref().on("value", function(snapshot){
-
-
-
-  })
       
+var uerRef = localStorage.getItem("userRef")
 
-  //book data obj
-  var bookData = {
-      image: image,
-      title: title,
-      author: author
-  }
-  
-  database.ref().set(bookData);
+if (!userRef) {
+    database.ref().push({
 
+        books: [],
+    }
 
+    ).then(function(snapshot) {
+        console.log(snapshot.key);
+        userRef = snapshot.key
+        localStorage.setItem("userRef", snapshot.key)
 
-
-
-
-
-
+    });
+}
 
 
+database.ref(userRef).on("value", function(snapshot){
+    console.log(books);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
   
 
-  //   // login js
-//   // FirebaseUI config.
-//   var uiConfig = {
-//     signInFlow: 'popup',
-//     signInSuccessUrl: '<url-to-redirect-to-on-success>',
-//     signInOptions: [
-//       // Leave the lines as is for the providers you want to offer your users.
-//       //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-//       //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-//       //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-//       //firebase.auth.GithubAuthProvider.PROVIDER_ID,
-//       firebase.auth.EmailAuthProvider.PROVIDER_ID,
-//       //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-//       //firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
-//     ],
-//     // tosUrl and privacyPolicyUrl accept either url string or a callback
-//     // function.
-//     // Terms of service url/callback.
-//     tosUrl: '<index.html>',
-//     // Privacy policy url/callback.
-//     privacyPolicyUrl: function() {
-//       window.location.assign('index.html');
-//     }
-//   };
+//click function for add books
+    //get all the info from object
+database.ref(userRef + "/books").push("#readList")
 
-//   // Initialize the FirebaseUI Widget using Firebase.
-//   var ui = new firebaseui.auth.AuthUI(firebase.auth());
-//   // The start method will wait until the DOM is loaded.
-//   ui.start('#firebaseui-auth-container', uiConfig);
-  
 
-//   function startAuth(){
+database.ref(userRef + "/books").on("child_added")
 
-//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
