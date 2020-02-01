97194315cec363152aaee8c1f268f10217d8f6ea
Brandon Williams <brando5393@gmail.com>
2020-02-01 11:57:05 -0600

    aded firebase connnecting code

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCtuSfxpVxINhIyPIpuMGAHswNVvPYAgeM",
+    authDomain: "coding-bootcamp-project-986ec.firebaseapp.com",
+    databaseURL: "https://coding-bootcamp-project-986ec.firebaseio.com",
+    projectId: "coding-bootcamp-project-986ec",
+    storageBucket: "coding-bootcamp-project-986ec.appspot.com",
+    messagingSenderId: "25197836680",
+    appId: "1:25197836680:web:08de31c393589bc2cfe23d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
