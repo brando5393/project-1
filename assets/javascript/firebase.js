26851bf8aaa0073900f1bc43c4ebd38ad48f85cf
Brandon Williams <brando5393@gmail.com>
2020-02-01 13:04:49 -0600

    SCRAPING USER AUTH FOR NOW

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -9,41 +9,91 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  
+  
+  var database = firebase.database();
+  
+  
+  
+  // firebase code to store reading lists
+  database.ref().on("value", function(snapshot){
+
+
+
+  })
+      
+
+  //book data obj
+  var bookData = {
+      image: image,
+      title: title,
+      author: author
+  }
+  
+  database.ref().set(bookData);
 
 
 
 
-  // login js
-  // FirebaseUI config.
-  var uiConfig = {
-    signInFlow: 'popup',
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      //firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-      //firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
-    ],
-    // tosUrl and privacyPolicyUrl accept either url string or a callback
-    // function.
-    // Terms of service url/callback.
-    tosUrl: '<index.html>',
-    // Privacy policy url/callback.
-    privacyPolicyUrl: function() {
-      window.location.assign('index.html');
-    }
-  };
 
-  // Initialize the FirebaseUI Widget using Firebase.
-  var ui = new firebaseui.auth.AuthUI(firebase.auth());
-  // The start method will wait until the DOM is loaded.
-  ui.start('#firebaseui-auth-container', uiConfig);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
 
-  function startAuth(){
+  //   // login js
+//   // FirebaseUI config.
+//   var uiConfig = {
+//     signInFlow: 'popup',
+//     signInSuccessUrl: '<url-to-redirect-to-on-success>',
+//     signInOptions: [
+//       // Leave the lines as is for the providers you want to offer your users.
+//       //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+//       //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+//       //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+//       //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+//       firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//       //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+//       //firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+//     ],
+//     // tosUrl and privacyPolicyUrl accept either url string or a callback
+//     // function.
+//     // Terms of service url/callback.
+//     tosUrl: '<index.html>',
+//     // Privacy policy url/callback.
+//     privacyPolicyUrl: function() {
+//       window.location.assign('index.html');
+//     }
+//   };
 
-  }
+//   // Initialize the FirebaseUI Widget using Firebase.
+//   var ui = new firebaseui.auth.AuthUI(firebase.auth());
+//   // The start method will wait until the DOM is loaded.
+//   ui.start('#firebaseui-auth-container', uiConfig);
+  
+
+//   function startAuth(){
+
+//   }
+
